d5286a796df2f88d069b878f15dd5616bdc7a614
Omprakash <Sonawaneom617@gmail.com>
2025-11-24 00:32:46 +0530

    Created multiple environment like qa,uat,dev and also created the ConfigManager class
    Updated the pom.xml file as java version 14

--- src/test/java/com/api/pojo/UserCredentials.java
+++ src/test/java/com/api/pojo/UserCredentials.java
@@ -1,4 +1,4 @@
-package com.api.test;
+package com.api.pojo;
 
 public class UserCredentials {
 
--- src/test/java/com/api/test/LoginAPIRequestTest.java
+++ src/test/java/com/api/test/LoginAPIRequestTest.java
@@ -4,21 +4,29 @@ import static io.restassured.RestAssured.*;
 
 import static org.hamcrest.Matchers.*;
 
+import java.io.IOException;
+
 import org.testng.annotations.Test;
+
+import com.api.pojo.UserCredentials;
+
+import static com.api.utils.ConfigManager.*;
 
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 
-public class LoginAPIRequest {
+public class LoginAPIRequestTest {
 
 	@Test
-	public void loginAPITest() {
+	public void loginAPITest1() throws IOException {
 		
+		
+		System.out.println(System.getProperty("env"));
 		UserCredentials userCredentials =new UserCredentials("iamfd","password");
 		
 		
 		given()
-		.baseUri("http://64.227.160.186:9000/v1")
+		.baseUri(getProperty("BASE_URI"))
 		.and()
 		.contentType(ContentType.JSON)
 		.and()
