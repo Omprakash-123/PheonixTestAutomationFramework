11e18753a46cc2aaa20c8ec9ba0f90436afb8e19
Omprakash <Sonawaneom617@gmail.com>
2025-11-29 01:42:22 +0530

    Created SpecUtil to handle the common api request config and assertion

--- src/test/java/com/api/test/CountAPITest.java
+++ src/test/java/com/api/test/CountAPITest.java
@@ -4,6 +4,8 @@ import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
 import com.api.constant.Role;
+import com.api.utils.SpecUtil;
+
 import static com.api.utils.ConfigManager.*;
 
 import io.restassured.module.jsv.JsonSchemaValidator;
@@ -21,19 +23,12 @@ public class CountAPITest {
 	public void verifyCountAPIResponse() throws IOException {
 		
 		given()
-		.baseUri(getProperty("BASE_URI"))
-		.and()
-		.header("Authorization",getToken(Role.FD))
-		.log().uri()
-		.log().method()
-		.log().headers()
+		.spec(SpecUtil.requestSpecWithAuth())
 		.when()
 		.get("/dashboard/count")
 		.then()
-		.log().all()
-		.statusCode(200)
+		.spec(SpecUtil.responseSpec_OK())
 		.body("message", equalTo("Success"))
-		.time(lessThan(1000L))
 		.body("data",notNullValue())
 		.body("data.size()",equalTo(3))
 		.body("data.count", everyItem(greaterThanOrEqualTo(0)))
@@ -45,12 +40,11 @@ public class CountAPITest {
 	public void countAPITest_MissingAuthToken() throws IOException {
 		
 		given()
-		.baseUri(getProperty("BASE_URI"))
-		.and()
+		.spec(SpecUtil.requestSpec())
 		.when()
 		.get("/dashboard/count")
 		.then()
-		.statusCode(401);
+		.spec(SpecUtil.responseSpec_TEXT(401));
 	}
 	
 
