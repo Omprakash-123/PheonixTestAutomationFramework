f462613da36a9a925a819c5676c50dd3d9ada8a9
Omprakash <Sonawaneom617@gmail.com>
2025-11-29 16:01:47 +0530

    changes in util

--- src/test/java/com/api/utils/DateTimeUtil.java
+++ src/test/java/com/api/utils/DateTimeUtil.java
@@ -2,18 +2,19 @@ package com.api.utils;
 
 import static java.time.temporal.ChronoUnit.*;
 
+import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 import org.joda.time.Instant;
 
 public class DateTimeUtil {
 
-	
 	private DateTimeUtil() {
-		
+
 	}
-	
-	public static void getTimeWithDaysAgo(int days) {
-		   Instant.now().minus(days,ChronoUnit.DAYS);	
-		   }
+
+	public static String getTimeWithDaysAgo(long days) {
+		return Instant.now().minus(days).toString();
+	}
+
 }
